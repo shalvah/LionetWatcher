fde859913f9b8206ae62f507f0f3acee1ea8cb23
Shalvah <diakon.ng@gmail.com>
2017-01-29 13:45:31 +0100

    Initial commit

---
+++ app/src/main/java/me/shalvah/lionetwatcher/WifiReceiver.java
@@ -0,0 +1,20 @@
+package me.shalvah.lionetwatcher;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class WifiReceiver extends BroadcastReceiver
+	{
+		public WifiReceiver()
+		{
+		}
+
+		@Override
+		public void onReceive(Context context, Intent intent)
+		{
+			// TODO: This method is called when the BroadcastReceiver is receiving
+			// an Intent broadcast.
+			throw new UnsupportedOperationException("Not yet implemented");
+		}
+	}
