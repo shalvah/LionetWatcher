1a40b9c4c6a4cbe2960292fa0a52694ac918affc
Shalvah <diakon.ng@gmail.com>
2017-01-29 16:47:28 +0100

    Get Wifi details and speed

--- app/src/main/java/me/shalvah/lionetwatcher/WifiReceiver.java
+++ app/src/main/java/me/shalvah/lionetwatcher/WifiReceiver.java
@@ -3,6 +3,10 @@ package me.shalvah.lionetwatcher;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
 
 public class WifiReceiver extends BroadcastReceiver
 	{
@@ -13,8 +17,16 @@ public class WifiReceiver extends BroadcastReceiver
 		@Override
 		public void onReceive(Context context, Intent intent)
 		{
-			// TODO: This method is called when the BroadcastReceiver is receiving
-			// an Intent broadcast.
-			throw new UnsupportedOperationException("Not yet implemented");
+			NetworkInfo ni = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+			if (ni.getState() == NetworkInfo.State.CONNECTED)
+			{
+				Log.d("WIFI_INFO: ", "" + ni.toString());
+				WifiManager wm = (WifiManager) context.getSystemService(Context.WIFI_SERVICE) ;
+				WifiInfo wi = wm.getConnectionInfo();
+				if (wi.getSSID().startsWith("LIONET"))
+				{
+					Log.d("WIFI_SPEED", "" + wi.getLinkSpeed());
+				}
+			}
 		}
 	}
